124bccb6fafe7587f24a384a19a606aeeec240ac
onekx <1095716578@qq.com>
2020-07-19 16:56:15 +0800

    修改求职者个人中心

--- front-end/src/components/user/EmployeeProfile.vue
+++ front-end/src/components/user/EmployeeProfile.vue
@@ -1,48 +1,45 @@
 <template>
-  <div class="content">
-    <el-form ref="form" label-width="80px">
-      <el-form-item label="姓名">
-        <el-input v-model="name" maxlength="5" clearable placeholder="请输入姓名"></el-input>
-      </el-form-item>
-      <el-form-item label="性别">
-        <el-select v-model="gender" placeholder="请选择性别">
-          <el-option label="男" value="man"></el-option>
-          <el-option label="女" value="woman"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="年龄">
-        <el-input v-model="age" placeholder="请输入年龄"></el-input>
-      </el-form-item>
-      <el-form-item label="毕业院校">
-        <el-input v-model="university" placeholder="请输入毕业院校"></el-input>
-      </el-form-item>
-      <el-form-item label="专业">
-        <el-input v-model="subject" placeholder="请输入所学专业"></el-input>
-      </el-form-item>
-      <el-form-item label="学历">
-        <el-select v-model="degree" placeholder="请选择学历">
-          <el-option label="专科" value="associate"></el-option>
-          <el-option label="本科" value="bachelor"></el-option>
-          <el-option label="研究生" value="master"></el-option>
-          <el-option label="博士生" value="doctor"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="个人简介">
-        <el-input
-          type="textarea"
-          v-model="desc"
-          maxlength="50"
-          placeholder="请输入个人简介（五十字以内）"
-          style="width: 500px;"
-        ></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">确认修改</el-button>
-        <el-button @click="onCancel">取消</el-button>
-        <el-button type="danger" class="logout-btn" @click="logout">退出登录</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
+  <el-form ref="form" label-width="80px">
+    <el-form-item label="姓名">
+      <el-input v-model="name" maxlength="5" clearable placeholder="请输入姓名"></el-input>
+    </el-form-item>
+    <el-form-item label="性别">
+      <el-select v-model="gender" placeholder="请选择性别">
+        <el-option label="男" value="man"></el-option>
+        <el-option label="女" value="woman"></el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item label="年龄">
+      <el-input v-model="age" placeholder="请输入年龄"></el-input>
+    </el-form-item>
+    <el-form-item label="毕业院校">
+      <el-input v-model="university" placeholder="请输入毕业院校"></el-input>
+    </el-form-item>
+    <el-form-item label="专业">
+      <el-input v-model="subject" placeholder="请输入所学专业"></el-input>
+    </el-form-item>
+    <el-form-item label="学历">
+      <el-select v-model="degree" placeholder="请选择学历">
+        <el-option label="专科" value="associate"></el-option>
+        <el-option label="本科" value="bachelor"></el-option>
+        <el-option label="研究生" value="master"></el-option>
+        <el-option label="博士生" value="doctor"></el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item label="个人简介">
+      <el-input
+        type="textarea"
+        v-model="desc"
+        maxlength="50"
+        placeholder="请输入个人简介（五十字以内）"
+        style="width: 500px;"
+      ></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">确认修改</el-button>
+      <el-button @click="onCancel">取消</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
@@ -79,15 +76,6 @@ export default {
         console.log(err)
       }
     },
-    logout() {
-      window.localStorage.removeItem('token')
-      this.$store.commit({
-        type: 'initialState',
-        id: '',
-        role: ''
-      })
-      this.$router.push('/')
-    },
     onCancel() {
       Object.assign(this.$data, this.$options.data())
     }
@@ -111,23 +99,6 @@ export default {
 </script>
 
 <style scoped>
-.content {
-  width: 1000px;
-  margin: 30px auto;
-  background-color: #fff;
-  padding: 20px;
-  border-radius: 5px;
-  box-sizing: border-box;
-}
-
-.line {
-  margin: 0 10px;
-}
-
-.logout-btn {
-  float: right;
-}
-
 .el-form {
   width: 580px;
   margin: 0 auto;
--- front-end/src/pages/Employee.vue
+++ front-end/src/pages/Employee.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="main">
     <Header />
-    <EmployeeProfile />
+    <div class="content">
+      <el-tabs :tab-position="tabPosition">
+        <el-tab-pane label="用户信息">
+          <EmployeeProfile />
+        </el-tab-pane>
+        <el-tab-pane label="投过简历的公司">投过简历的公司</el-tab-pane>
+        <el-tab-pane label="退出登录">退出登录</el-tab-pane>
+      </el-tabs>
+    </div>
   </div>
 </template>
 
@@ -14,6 +22,22 @@ export default {
   components: {
     Header,
     EmployeeProfile
+  },
+  data() {
+    return {
+      tabPosition: 'left'
+    }
   }
 }
 </script>
+
+<style scoped>
+.content {
+  width: 1000px;
+  margin: 30px auto;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-sizing: border-box;
+}
+</style>
